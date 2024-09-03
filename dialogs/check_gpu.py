8657c61e55c572d36c3ade7b489ac208467b7cfb
htulet <hadrien.tulet@cirad.fr>
2024-09-03 16:38:50 +0200

    fixed cuda version check ; problem pip torch

--- dialogs/check_gpu.py
+++ dialogs/check_gpu.py
@@ -4,13 +4,17 @@ import platform
 def check_nvidia_gpu():
     try:
         # Run the nvidia-smi command and capture the output
-        output = subprocess.check_output(["nvidia-smi", "--query-gpu=name,driver_version,cuda_version", "--format=csv,noheader"], stderr=subprocess.STDOUT)
+        output = subprocess.check_output(["nvidia-smi", "--query-gpu=name,driver_version", "--format=csv,noheader"], stderr=subprocess.STDOUT)
         output = output.decode("utf-8").strip()
         
         # Parse the output
         gpu_info = output.split(',')
         gpu_name = gpu_info[0].strip()
-        cuda_version = gpu_info[2].strip()
+        
+        output_cuda_version = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        for line in output_cuda_version.stdout.split('\n'):
+            if 'CUDA Version' in line:
+                cuda_version = line.split('CUDA Version: ')[1].split()[0]
         
         return True, gpu_name, cuda_version
     except (subprocess.CalledProcessError, FileNotFoundError):
