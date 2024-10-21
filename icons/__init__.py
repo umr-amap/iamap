3c9d2e2791e72efbdcb5d0dccd79847c5b2eecb0
ptresson <paul.tresson@ird.fr>
2024-10-21 15:31:43 +0200

    update icons

--- icons/__init__.py
+++ icons/__init__.py
@@ -2,12 +2,11 @@ import os
 from PyQt5.QtGui import QIcon
 
 cwd = os.path.abspath(os.path.dirname(__file__))
-# encoder_tool_path = os.path.join(cwd, 'encoder_tool.svg')
-encoder_tool_path = os.path.join(cwd, 'network-base-solid-svgrepo-com.svg')
-reduction_tool_path = os.path.join(cwd, 'chart-scatterplot-solid-svgrepo-com.svg')
-cluster_tool_path = os.path.join(cwd, 'chart-scatter-3d-svgrepo-com.svg')
-similarity_tool_path = os.path.join(cwd, 'scatter-plot-svgrepo-com.svg')
-random_forest_tool_path = os.path.join(cwd, 'forest-svgrepo-com.svg')
+encoder_tool_path = os.path.join(cwd, 'encoder.svg')
+reduction_tool_path = os.path.join(cwd, 'proj.svg')
+cluster_tool_path = os.path.join(cwd, 'cluster.svg')
+similarity_tool_path = os.path.join(cwd, 'sim.svg')
+random_forest_tool_path = os.path.join(cwd, 'forest.svg')
 
 QIcon_EncoderTool = QIcon(encoder_tool_path)
 QIcon_ReductionTool = QIcon(reduction_tool_path)
