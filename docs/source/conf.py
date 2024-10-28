a64b71dc8c50ed715b6069d6b7033af0202d9b4a
ptresson <paul.tresson@ird.fr>
2024-10-28 17:14:47 +0100

    get version and authors from metadata.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,14 +5,42 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import re
+
+
+metadata_file_path = os.path.join('..', '..', 'metadata.txt')
+metadata_file_path = os.path.abspath(metadata_file_path)
+with open(metadata_file_path, 'rt') as file:
+    file_content = file.read()
+try:
+    versions_from_metadata = re.findall(r'version=(.*)', file_content)[0]
+except Exception as e:
+    raise Exception("Failed to read version from metadata!")
+
+try:
+    author_from_metadata = re.findall(r'author=(.*)', file_content)[0]
+except Exception as e:
+    raise Exception("Failed to read author from metadata!")
+
+try:
+    name_from_metadata = re.findall(r'name=(.*)', file_content)[0]
+except Exception as e:
+    raise Exception("Failed to read name from metadata!")
 
 project = "iamap"
 copyright = "2024, TRESSON Paul, TULET Hadrien, LE COZ Pierre"
-author = "TRESSON Paul, TULET Hadrien, LE COZ Pierre"
-release = "0.5.9"
+author = author_from_metadata
+# The short X.Y version
+version = versions_from_metadata
+# The full version, including alpha/beta/rc tags
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+
 
 
 extensions = [
