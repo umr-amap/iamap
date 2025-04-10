9f670cec6a9dad87ab4d48a9feae23ba20f89d67
ptresson <paul.tresson@ird.fr>
2025-04-10 18:03:05 +0200

    add util to manually change ViT input layers

--- utils/trch.py
+++ utils/trch.py
@@ -20,3 +20,28 @@ def quantize_model(model, device):
             model, {nn.Linear}, dtype=torch.qint8
         )
     return model
+
+def vit_first_layer_with_nchan(model, in_chans=1):
+
+    kernel_size = model.patch_embed.proj.kernel_size
+    stride = model.patch_embed.proj.stride
+    embed_dim = model.patch_embed.proj.out_channels # corresponds to embed_dim
+    # copy the original patch_embed.proj config 
+    # except the number of input channels
+    new_conv = torch.nn.Conv2d(
+            in_chans, 
+            out_channels=embed_dim,
+            kernel_size=kernel_size, 
+            stride=stride
+            )
+    # copy weigths and biases
+    weight = model.patch_embed.proj.weight.clone()
+    bias = model.patch_embed.proj.bias.clone()
+    with torch.no_grad():
+        for i in range(0,in_chans):
+            j = i%3 # cycle every 3 bands
+            new_conv.weight[:,i,:,:] = weight[:,j,:,:] #band i takes old band j (blue) weights
+            new_conv.bias[:] = bias[:]
+    model.patch_embed.proj = new_conv
+
+    return model
