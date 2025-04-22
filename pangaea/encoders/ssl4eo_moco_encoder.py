8ff35297d18a73562b4346d1c7e3c98d30bdbc7d
ptresson <paul.tresson@ird.fr>
2025-04-22 15:24:03 +0200

    relative import in class def for SSL4EO_MOCO_Encoder

--- pangaea/encoders/ssl4eo_moco_encoder.py
+++ pangaea/encoders/ssl4eo_moco_encoder.py
@@ -8,7 +8,8 @@ import torch
 import torch.nn as nn
 from timm.models.vision_transformer import Block, PatchEmbed
 
-from pangaea.encoders.base import Encoder
+# from pangaea.encoders.base import Encoder
+from .base import Encoder
 
 
 class SSL4EO_MOCO_Encoder(Encoder):
